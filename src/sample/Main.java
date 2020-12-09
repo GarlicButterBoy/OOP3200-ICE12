17f7e11bfca2a364856a73a506e8c8ca593b27a5
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2020-12-09 11:47:48 -0500

    Made a VERY simple JavaFX Program!

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,6 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,13 +23,29 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         primaryStage.setTitle("Hello World");
         //Step 1 - Create a Control
         Label helloLabel = new Label("Hello Worlds!");
         Label goodbyeLabel = new Label("Goodbye...For now!");
         Button clickMeButton = new Button("Click Me!");
+        Font font = Font.font("Verdana", FontWeight.BOLD, 40.0);
 
+        //Step 1.5 - Configure newly created Controls
+        clickMeButton.setFont(font);
+
+        //Step 1.9 - Event Type Controls!
+        clickMeButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent actionEvent)
+            {
+
+                System.out.println("ClickmeButton was compressed! Thats pretty cool!");
+                helloLabel.setText(goodbyeLabel.getText());
+                goodbyeLabel.setText(helloLabel.getText());
+            }
+        });
 
         //Step 2 - Create a Container
         //HBox hbox = new HBox(helloLabel, clickMeButton);
@@ -34,7 +54,7 @@ public class Main extends Application {
         GridPane grid = new GridPane();
         grid.add(helloLabel, 1, 1);
         grid.add(clickMeButton, 2,2);
-        grid.add(goodbyeLabel, 3, 3);
+       // grid.add(goodbyeLabel, 3, 3);
 
         //Step 3 - Create a Scene
         Scene primaryScene = new Scene(grid, WIDTH, HEIGHT);
